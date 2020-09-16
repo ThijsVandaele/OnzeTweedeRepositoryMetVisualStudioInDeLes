d99fde70d4a35b9792ce4c76bf4ec12439cd4c02
Thijs Vandaele <thijs.vandaele@live.be>
2020-09-16 20:24:04 +0200

    Revert "Add simple summation"
    This reverts commit c3c0f46aaf4f6da0def8036fa2db3d8299f4d33d.

--- OnzeTweedeRepositoryMetVisualStudio/Program.cs
+++ OnzeTweedeRepositoryMetVisualStudio/Program.cs
@@ -6,12 +6,7 @@ namespace OnzeTweedeRepositoryMetVisualStudio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Geef een getal");
-            int getal = ConsoleHelper.GetInteger();
-
-            int som = getal * getal;
-
-            Console.WriteLine($"De som is: {som}");
+            Console.WriteLine("Hello World!");
         }
     }
 }
