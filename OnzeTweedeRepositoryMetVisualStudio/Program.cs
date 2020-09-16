be25184218869b10cca695b37455886671eec643
Thijs Vandaele <thijs.vandaele@live.be>
2020-09-16 20:48:07 +0200

    remove line

--- OnzeTweedeRepositoryMetVisualStudio/Program.cs
+++ OnzeTweedeRepositoryMetVisualStudio/Program.cs
@@ -6,7 +6,7 @@ namespace OnzeTweedeRepositoryMetVisualStudio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
